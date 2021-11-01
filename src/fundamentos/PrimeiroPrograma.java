fed516eefcea59603ccb24576fa964097a51f233
Uislan Cruz <uislansk8@hotmail.com>
2021-11-01 15:26:13 -0300

    Create PrimeiroPrograma.java

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		int n=1;
+		int n2=2;
+		int s= n + n2;
+		
+		System.out.println(s);
+		
+	}
+	
+
+}
+

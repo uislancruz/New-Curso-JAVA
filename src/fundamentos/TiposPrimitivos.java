e68145a50ea413f1e5d48b29a55de89129af8be4
Uislan Cruz <uislansk8@hotmail.com>
2021-11-02 14:04:37 -0300

    Update TiposPrimitivos.java

--- src/fundamentos/TiposPrimitivos.java
+++ src/fundamentos/TiposPrimitivos.java
@@ -1,5 +1,28 @@
 package fundamentos;
 
+
 public class TiposPrimitivos {
+	public static void main(String[] args) {
+		
+		//tipos numericos inteiros
+		byte anosDeempresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56789;
+		long pontosAcumulados = 3_134_845_223L;
+		
+		//tipoos numericos reais:
+		
+		float salario = 11_445.44F;
+		double vendasAcumuladas = 2_991_797+103.01;
+		
+		//tipo booleano
+		boolean estaDeFerias = false; ///true
+		
+		//tipo caractere
+		char status = 'A'; // ativo
+		
+		//dias de empresa
+		System.out.println( );
+	}
 
 }

0a617b209c8583bcc1ac1af3b994c048274a032c
Uislan Cruz <uislansk8@hotmail.com>
2021-11-02 13:17:04 -0300

    Update Temperatura.java

--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -3,11 +3,17 @@ package fundamentos;
 public class Temperatura {
 	
 	public static void main(String[] args) {
-		final double diferenca = -32;
-		double constante = 5;
-		double divisor = 9;
-		double c = (-32) * constante/divisor;
-		System.out.println(c);
+		final double fator = 5.0 / 9.0;
+		final double ajuste = 32;
+		
+		double fahrenheit = 86;
+		double celsius = (fahrenheit - ajuste) * fator;
+		
+		System.out.println("O resultado é: " +celsius+"º");
+		
+		fahrenheit = 150;
+		celsius = (fahrenheit - ajuste) * fator;
+		System.out.println("O resultado é: "+celsius+"ºC");
 	}
 
 }

2762f72056dc113f4693be0a061305b416a9709f
Uislan Cruz <uislansk8@hotmail.com>
2021-11-03 23:15:41 -0300

    Update ConversaoTipoPrimitivoNumerico.java

--- src/fundamentos/ConversaoTipoPrimitivoNumerico.java
+++ src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -2,11 +2,13 @@ package fundamentos;
 
 public class ConversaoTipoPrimitivoNumerico {
 	public static void main(String[] args) {
-		double a = 1;
+		double a = 1; //implicita
 		System.out.println(a);
 		
-		float b = 1.0f;
+		float b = (float)1.0; //explicita
 		System.out.println(b);
+		
+		bigdecimal b = 1;
 	}
 
 }

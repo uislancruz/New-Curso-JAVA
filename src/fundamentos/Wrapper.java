355895444144bccd0be8299959b84e0b12699933
Uislan Cruz <uislansk8@hotmail.com>
2021-11-03 21:53:30 -0300

    Create Wrapper.java

---
+++ src/fundamentos/Wrapper.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		//byte
+		byte b = 100;
+		short s = 1000;
+		
+		Boolean bo = true;
+		System.out.println(bo.parseBoolean("t"));
+
+	}
+
+}

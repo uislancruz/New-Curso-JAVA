2f89317d558b3dfa5a8e831c0e2f5ef483e1ae0e
Uislan Cruz <uislansk8@hotmail.com>
2021-11-08 20:54:28 -0300

    Update DesafioLogicos.java

--- src/fundamentos/operadores/DesafioLogicos.java
+++ src/fundamentos/operadores/DesafioLogicos.java
@@ -1,5 +1,18 @@
 package fundamentos.operadores;
 
+import java.util.Scanner;
+
 public class DesafioLogicos {
+	public static void main(String[] args) {
+		//trabalho na terça(V ou F)
+		//trabalho na quinda(V ou F)
+		
+		boolean trabalho1 = true;
+		boolean trabalho2 = true;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		
+	}
 
 }

6e52ca3e9eb9818ecbbd3fc60e253135f1a103a6
Uislan Cruz <uislansk8@hotmail.com>
2021-11-08 21:21:18 -0300

    Update DesafioLogicos.java

--- src/fundamentos/operadores/DesafioLogicos.java
+++ src/fundamentos/operadores/DesafioLogicos.java
@@ -9,10 +9,27 @@ public class DesafioLogicos {
 		
 		boolean trabalho1 = true;
 		boolean trabalho2 = true;
+
+		//System.out.println("Digite 1 para sim \nDigite 2 para não");
 		
 		Scanner teclado = new Scanner(System.in);
+			
+		
+		System.out.println("Informe o numero de empregos que voce passou?: ");
+		int valor1 = teclado.nextInt();
+		
+		if (valor1 == 1) {
+			System.out.println("Iremos ao shopping comprar a TV de 32 e tomar sorvete");			
+		}
+			if(valor1 == 2) {
+				System.out.println("Iremos ao shopping comprar a TV de 50 polegadas e tomar sorvete");
+			}else if(valor1 == 0) {
+				System.out.println("Infelizmente ficaremos em casa :'( ");
+			}
+	}
+		
 		
 		
 	}
 
-}
+

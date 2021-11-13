667139824f8e46675526770562fb09b13634eda0
Uislan Cruz <uislansk8@hotmail.com>
2021-11-13 18:27:55 -0300

    Update If.java

--- src/controle/If.java
+++ src/controle/If.java
@@ -1,8 +1,19 @@
 package controle;
 
+import javax.swing.JOptionPane;
+
 public class If {
 	public static void main(String[] args) {
+	
+		String valor = JOptionPane.showInputDialog("Informe o Numero: ");
+		int numero = Integer.parseInt(valor);
 		
+		if (numero %2 == 0) {
+			System.out.println("Numero par");
+		}else {
+			System.out.println("Numero Impar");
+		}
 	}
+	
 
 }

48e7e1fa467eb2def80609800b00b9d60d5bb18c
Uislan Cruz <uislansk8@hotmail.com>
2021-11-13 18:38:13 -0300

    Update IfElseIf.java

--- src/controle/IfElseIf.java
+++ src/controle/IfElseIf.java
@@ -13,12 +13,15 @@ public class IfElseIf {
 		
 		if(nota > 10 || nota < 0) {
 			System.out.println("Nota invalida");
-		}else {
-			if (nota >= 8.1) {
-				
+		}else if (nota >= 8.1) {
+			 				
 				System.out.println("Conceito A");
+			}else {
+				if (nota >= 6.1) {
+					System.out.println("Conceito B");
+				}
 			}
-		}
+		
 		
 		entrada.close();
 		

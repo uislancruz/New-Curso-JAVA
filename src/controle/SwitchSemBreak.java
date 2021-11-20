3ecb0a60fcd82313f10db70b3b7223e0a49c3bb0
Uislan Cruz <uislansk8@hotmail.com>
2021-11-20 16:15:18 -0300

    Update SwitchSemBreak.java

--- src/controle/SwitchSemBreak.java
+++ src/controle/SwitchSemBreak.java
@@ -1,5 +1,24 @@
 package controle;
 
 public class SwitchSemBreak {
+	public static void main(String[] args) {
+		
+		String faixa = "vermelha";
+		
+		switch(faixa.toLowerCase()) {
+		case "preta":
+			System.out.println("Sei o Bassai-Dai...");
+		case "marrom":
+			System.out.println("Sei o Tekki Shodan");
+		case "roxa":
+			System.out.println("Sei o Shasam");
+		case "verde":
+			System.out.println("Sei o Capitão mavel");
+		case "laranja":
+			System.out.println("Sei o tsubasa");
+		case "vermelha":
+			System.out.println("Sei o Tekki ");
+		}
+	}
 
 }

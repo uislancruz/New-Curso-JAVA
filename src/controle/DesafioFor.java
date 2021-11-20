1a1a601dfa22f8e05abdf3101049e26b60b9b385
Uislan Cruz <uislansk8@hotmail.com>
2021-11-20 15:48:58 -0300

    Update DesafioFor.java

--- src/controle/DesafioFor.java
+++ src/controle/DesafioFor.java
@@ -1,5 +1,19 @@
 package controle;
 
 public class DesafioFor {
+	public static void main(String[] args) {
+		
+		String valor = "#";
+		int laco1 = 1;
+		int laco2 = 5;
+		
+		for(int i = laco1; i <= laco2; i++) {
+		//for(int i = 1; i<=5;i++) {
+			System.out.println(valor);
+			
+			valor += "#";
+		}
+		
+	}
 
 }

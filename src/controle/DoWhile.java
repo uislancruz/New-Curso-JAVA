14c57e9bb00368f83f662a00e68392480cc39344
Uislan Cruz <uislansk8@hotmail.com>
2021-11-20 14:04:59 -0300

    Update DoWhile.java

--- src/controle/DoWhile.java
+++ src/controle/DoWhile.java
@@ -1,5 +1,23 @@
 package controle;
 
+import java.util.Scanner;
+
 public class DoWhile {
+	public static void main(String[] args) {
+		
+		//if(condição enquanto); sentenca; ou{} 
+		//while(...) sentenca; ou{}
+		//for(inicialização de variavel;expressao; modificaçao da variavel) sentenca;ou{}
+		
+		//do {} while(expressoa); --> unica que termina com ";"
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		do {
+			
+		}while(false);
+		
+		entrada.close();
+	}
 
 }

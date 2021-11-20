6ba14c5e8aa3abd14c69162b72f7fd778ec1e971
Uislan Cruz <uislansk8@hotmail.com>
2021-11-20 15:56:26 -0300

    Update DesafioFor.java

--- src/controle/DesafioFor.java
+++ src/controle/DesafioFor.java
@@ -4,14 +4,20 @@ public class DesafioFor {
 	public static void main(String[] args) {
 		
 		String valor = "#";
-		int laco1 = 1;
-		int laco2 = 5;
+		//int laco1 = 1;
+		//int laco2 = 5;
 		
-		for(int i = laco1; i <= laco2; i++) {
-		//for(int i = 1; i<=5;i++) {
+		//for(int i = laco1; i <= laco2; i++) {
+		for(int i = 1; i<=5;i++) {
 			System.out.println(valor);
 			
 			valor += "#";
+		}
+		
+		//Fazendo um for sem usar numeros, apenas string
+		
+		for(String v = "#";!v.equals("######"); v+="#") {
+			System.out.println(v);
 		}
 		
 	}

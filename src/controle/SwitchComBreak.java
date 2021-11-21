74a6c7299e2286e7dcfa3c21ae709231f1c68b66
Uislan Cruz <uislansk8@hotmail.com>
2021-11-21 13:54:40 -0300

    Switch/Break
    Usando agora o switch com break (mais utilizado)

--- src/controle/SwitchComBreak.java
+++ src/controle/SwitchComBreak.java
@@ -1,8 +1,25 @@
 package controle;
 
+import java.util.Scanner;
+
 public class SwitchComBreak {
 	public static void main(String[] args) {
 		
+		String conceito = "";
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe a nota: ");
+		int nota = entrada.nextInt();
+		
+		switch(nota) {
+		case 10:
+			conceito = "A";
+			break;
+			
+		}
+		
+		System.out.println("Conceito é: "+ conceito);
+		entrada.close();
 	}
 
 }

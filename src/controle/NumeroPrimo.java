de32eb58d96efdcee7bd0a884e7690c209e98a05
Uislan Cruz <uislansk8@hotmail.com>
2021-11-21 15:53:30 -0300

    Resolução

---
+++ src/controle/NumeroPrimo.java
@@ -0,0 +1,30 @@
+package controle;
+
+import java.util.Scanner;
+
+public class NumeroPrimo {
+	public static void main(String[] args) {
+
+		int contadorDeDivisores = 0;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("\nDigite um numero para verificar se é primo:");
+		int numero = entrada.nextInt();
+
+		for (int i = 2; i < numero; i++) {
+			if (numero % i == 0) {
+				contadorDeDivisores++;
+			}
+		}
+
+		if (contadorDeDivisores == 0) {
+			System.out.println("\nO numero " + numero + " é primo.");
+		} else {
+			System.out.println("\nO numero " + numero + " não é primo.");
+		}
+
+		entrada.close();
+
+	}
+
+}

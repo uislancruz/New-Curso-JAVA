7ef72b2746719073b9736e3224c54ee4ea65b213
Uislan Cruz <uislansk8@hotmail.com>
2021-11-21 15:22:05 -0300

    Update ExFinal.java

--- src/controle/ExFinal.java
+++ src/controle/ExFinal.java
@@ -1,5 +1,35 @@
 package controle;
 
+import java.util.Scanner;
+
 public class ExFinal {
+	public static void main(String[] args) {
+		
+		System.out.println("Insira um numero de 0 até 10: ");
+		Scanner entrada = new Scanner(System.in);
+		int valor = entrada.nextInt();
+		
+		if (valor >= 0 && valor <= 10) {
+			if (valor%2 == 0) {
+				System.out.println("Valor é: " +valor+" e é um numero Par");
+			}else {
+				System.out.println("Valor é: " +valor+" e é um numero impar");
+			}
+		}
+		
+		
+	
+		entrada.close();
+	}
 
 }
+
+
+
+/*if (valor >= 0 && valor <= 10) {
+	if(valor%2 == 1) {
+		System.out.println("O valor é: " + valor + "É um numero par");
+	}else {
+		System.out.println("Valor digitado incorreto: ");
+	}
+}*/

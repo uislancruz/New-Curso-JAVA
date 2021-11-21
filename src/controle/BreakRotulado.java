9afd915e96e0a61a3e4156c69a794b7761cd3335
Uislan Cruz <uislansk8@hotmail.com>
2021-11-21 14:45:27 -0300

    Continue

--- src/controle/BreakRotulado.java
+++ src/controle/BreakRotulado.java
@@ -6,7 +6,7 @@ public class BreakRotulado {
 		//crianco rotulo para o parar o que eu determinar no caso o laço externo
 		externo: for(int i =0; i <3; i++) {
 			for (int j =0; j<3; j++) {
-				if(j==1) {
+				if(i==1) {
 					break externo;
 				}
 				System.out.printf("[%d %d]", i,j);

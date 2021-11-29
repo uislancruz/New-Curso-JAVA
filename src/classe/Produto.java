9855622a74e8f4dea3499e2daa6b339b757cbf74
Uislan Cruz <uislansk8@hotmail.com>
2021-11-28 21:53:25 -0300

    Classe 1

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -11,5 +11,9 @@ public class Produto {
 		return preco * (1 - desconto);
 		
 	}
+	
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+	}
 
 }
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -19,8 +19,8 @@ public class ProdutoTeste {
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
 		
-		double precoFinal1 = p1.precoComDesconto();
-		double precoFinal2 = p2.precoComDesconto();
+		double precoFinal1 = p1.precoComDesconto(0);
+		double precoFinal2 = p2.precoComDesconto(0.1);
 		double mediaCarrinho = ((precoFinal1 + precoFinal2)/2);
 		
 		System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);

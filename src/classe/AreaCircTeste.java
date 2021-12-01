9ec28722fe4bf6b3289e261b91f5c73abb2f38d0
Uislan Cruz <uislansk8@hotmail.com>
2021-11-30 22:54:43 -0300

    Update AreaCircTeste.java

--- src/classe/AreaCircTeste.java
+++ src/classe/AreaCircTeste.java
@@ -5,12 +5,16 @@ public class AreaCircTeste {
 		
 		AreaCirc a1 = new AreaCirc(10);
 		
-		a1.pi = 10;
-		System.out.println(a1.area());
+		//a1.pi = 10;
 		
 		AreaCirc a2 = new AreaCirc(5);
+		//a2.pi = 5;
 		
-		a2.pi = 5;
+		AreaCirc.pi = 3.14;
+		
+		System.out.println(a1.area());
+		
+		AreaCirc.pi = 0.1;
 		System.out.println(a2.area());
 	}
 

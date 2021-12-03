e486143fcfea44ac94f108c85a246279b7518df0
Uislan Cruz <uislansk8@hotmail.com>
2021-12-03 19:56:26 -0300

    Update ValorVsReferencia.java

--- src/classe/ValorVsReferencia.java
+++ src/classe/ValorVsReferencia.java
@@ -1,5 +1,16 @@
 package classe;
 
 public class ValorVsReferencia {
+	public static void main(String[] args) {
+		
+		double a = 2;
+		double b = a;
+		
+		a++;
+		b--;
+		
+		System.out.println(a + " " + b);
+		
+	}
 
 }

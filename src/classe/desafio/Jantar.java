5d5919820d2e49e99bce5580a02d56160397eaf6
Uislan Cruz <uislansk8@hotmail.com>
2021-12-04 14:49:19 -0300

    Update Jantar.java

--- src/classe/desafio/Jantar.java
+++ src/classe/desafio/Jantar.java
@@ -3,16 +3,16 @@ package classe.desafio;
 public class Jantar {
 	public static void main(String[] args) {
 		
-		Comida variavelcomida1 = new Comida("Arroz", 0.180);
-		Comida variavelcomida2 = new Comida("Feijão", 0.300);
+		Comida variavelComida1 = new Comida("Arroz", 0.180);
+		Comida variavelComida2 = new Comida("Feijão", 0.300);
 		
 		Pessoa variavelPessoa = new Pessoa("João", 99.8);
 		
 		System.out.println(variavelPessoa.apresentar());
-		variavelPessoa.comer(variavelcomida1);
+		variavelPessoa.comer(variavelComida1);
 		
 		System.out.println(variavelPessoa.apresentar());
-		variavelPessoa.comer(variavelcomida2);
+		variavelPessoa.comer(variavelComida2);
 		
 		System.out.println(variavelPessoa.apresentar());
 	}

507a87eb085e96a253fdd9b06302e736544baf73
Uislan Cruz <uislansk8@hotmail.com>
2021-12-07 22:56:46 -0300

    Update Exercicio.java

--- src/arrays/Exercicio.java
+++ src/arrays/Exercicio.java
@@ -15,7 +15,8 @@ public class Exercicio {
 		notasAlunoA[2] = 6.7;
 		
 		System.out.println(Arrays.toString(notasAlunoA));
-		
+		//codigo aprova de alterações, pq voce não ta usando numero fixo e sim quantidade de
+		//array (no caso) como referencia
 		double total = 0;
 		for(int i = 0; i < notasAlunoA.length; i++) {
 			total += notasAlunoA[i];
@@ -23,6 +24,12 @@ public class Exercicio {
 		
 		System.out.println(total/notasAlunoA.length);
 		
+		double [] notasAlunoB = {6.9, 8.9, 5.5, 10};
+		for (int i = 0; i < notasAlunoB.length; i++) {
+			total += notasAlunoB[i];
+		}
+		
+		System.out.println(total / notasAlunoB.length);
 	}
 
 }

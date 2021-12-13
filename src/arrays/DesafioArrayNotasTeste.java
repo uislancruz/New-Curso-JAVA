2f5b9cc1efb4eb3b05fdf468bd89e904b5b89529
Uislan Cruz <uislansk8@hotmail.com>
2021-12-13 20:33:34 -0300

    Update DesafioArrayNotasTeste.java

--- src/arrays/DesafioArrayNotasTeste.java
+++ src/arrays/DesafioArrayNotasTeste.java
@@ -1,5 +1,21 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class DesafioArrayNotasTeste {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Quantas notas:");
+		
+		int quantidadeNotas= entrada.nextInt();
+		
+		double [] notas = new double [quantidadeNotas];
+		System.out.println(quantidadeNotas);
+		
+		entrada.close();
+		
+	}
 
 }

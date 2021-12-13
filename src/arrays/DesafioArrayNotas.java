66bf22e67b03d53b0e51074462ea6f3a81db074d
Uislan Cruz <uislansk8@hotmail.com>
2021-12-13 20:33:38 -0300

    Update DesafioArrayNotas.java

--- src/arrays/DesafioArrayNotas.java
+++ src/arrays/DesafioArrayNotas.java
@@ -1,5 +1,22 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class DesafioArrayNotas {
+	
+	/*Scanner inserirNota = new Scanner(System.in);
+	
+	double []nota1 = {1};
+	
+		
+	DesafioArrayNotas(){
+				
+		if(nota1 == 1){
+			
+		}
+		
+	}*/
+
+	
 
 }

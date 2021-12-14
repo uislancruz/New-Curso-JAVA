00e1c55e044ab0e89533610f591f90a6fa9a9af9
Uislan Cruz <uislansk8@hotmail.com>
2021-12-13 21:54:02 -0300

    Update Matriz.java

--- src/arrays/Matriz.java
+++ src/arrays/Matriz.java
@@ -1,5 +1,32 @@
 package arrays;
 
+import java.util.Scanner;
 public class Matriz {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Quantos alunos? ");
+		int quantidadeAlunos = entrada.nextInt();
+		
+		System.out.println("Quantos notas por alunos? ");
+		int quantidadeNotas = entrada.nextInt();
+		
+		double [] [] notasDaTurma = new double [quantidadeAlunos][quantidadeNotas];
+		
+		double total = 0;
+		for(int aluno = 0; aluno <notasDaTurma.length; aluno++) {
+			for(int nota = 0; nota < notasDaTurma[aluno].length; nota++) {
+				System.out.printf("Informe a nota %d do aluno %d: ", nota, aluno);
+				notasDaTurma[aluno][nota] = entrada.nextDouble();
+				total+= notasDaTurma[aluno][nota];
+				
+			}
+			
+		}
+		double media = total / (quantidadeAlunos * quantidadeNotas);
+		System.out.println("A media da turma é: "+ media);
+		
+		entrada.close();
+	}
 
 }

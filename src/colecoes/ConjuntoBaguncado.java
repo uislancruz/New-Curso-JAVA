637952b0f0aa75b08597818486147482dd149036
Uislan Cruz <uislansk8@hotmail.com>
2021-12-23 11:09:49 -0300

    Update ConjuntoBaguncado.java

--- src/colecoes/ConjuntoBaguncado.java
+++ src/colecoes/ConjuntoBaguncado.java
@@ -1,5 +1,19 @@
 package colecoes;
 
+import java.util.HashSet;
+
 public class ConjuntoBaguncado {
+	public static void main(String[] args) {
+		HashSet conjunto = new HashSet();
+		
+		conjunto.add(1.2); //double -> Double
+		conjunto.add(true);
+		conjunto.add("Teste");
+		conjunto.add(1);
+		conjunto.add('x');
+		
+		System.out.println("Tamanho é " + conjunto.size());
+		
+	}
 
 }

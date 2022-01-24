72b6716eca9cd751953c73d687db6f72a2ec5085
Uislan Cruz <uislansk8@hotmail.com>
2022-01-24 20:45:37 -0300

    Update ConjuntoBaguncado.java

--- src/colecoes/ConjuntoBaguncado.java
+++ src/colecoes/ConjuntoBaguncado.java
@@ -6,13 +6,18 @@ public class ConjuntoBaguncado {
 	public static void main(String[] args) {
 		HashSet conjunto = new HashSet();
 		
-		conjunto.add(1.2); //double -> Double
-		conjunto.add(true);
+		conjunto.add(1.2); //double -> Double ( converte para uma classe)
+		conjunto.add(true); //boolean -> Boolean ( converte para uma classe)
 		conjunto.add("Teste");
 		conjunto.add(1);
 		conjunto.add('x');
 		
 		System.out.println("Tamanho é " + conjunto.size());
+		
+		//caso eu add um novo elemeto reptido, ele não alterará a quantidade do conjunto. 
+		//Ex:
+		
+		
 		
 	}
 

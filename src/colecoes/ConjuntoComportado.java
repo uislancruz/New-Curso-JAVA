6960615e32e82a11eb4cb318530b9a3d014ac0ed
Uislan Cruz <uislansk8@hotmail.com>
2022-01-24 21:17:48 -0300

    Update ConjuntoComportado.java

--- src/colecoes/ConjuntoComportado.java
+++ src/colecoes/ConjuntoComportado.java
@@ -1,5 +1,20 @@
 package colecoes;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class ConjuntoComportado {
+	public static void main(String[] args) {
+		Set<String> listaAprovados = new HashSet<String>();
+		//Set<String> lista = new HashSet<>(); posso deixar no hashset <> que ja copia: chamado de Diamond
+		listaAprovados.add("Ana");
+		listaAprovados.add("Carlos");
+		listaAprovados.add("Luca");
+		listaAprovados.add("Pedro");
+		
+		for(String nomeCandidato: listaAprovados) {
+			System.out.println(nomeCandidato);
+		}
+	}
 
 }

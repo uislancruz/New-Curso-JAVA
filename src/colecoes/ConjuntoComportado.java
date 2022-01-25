c97287a4e229ae6dc750568dd70d05bd476a58d9
Uislan Cruz <uislansk8@hotmail.com>
2022-01-24 21:24:26 -0300

    Update ConjuntoComportado.java

--- src/colecoes/ConjuntoComportado.java
+++ src/colecoes/ConjuntoComportado.java
@@ -1,12 +1,14 @@
 package colecoes;
 
-import java.util.Set;
-import java.util.HashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class ConjuntoComportado {
 	public static void main(String[] args) {
-		Set<String> listaAprovados = new HashSet<String>();
+		//Set<String> listaAprovados = new HashSet<String>();
 		//Set<String> lista = new HashSet<>(); posso deixar no hashset <> que ja copia: chamado de Diamond
+		SortedSet<String> listaAprovados = new TreeSet<String>();//Para manter a ordem com que foram add
+		
 		listaAprovados.add("Ana");
 		listaAprovados.add("Carlos");
 		listaAprovados.add("Luca");

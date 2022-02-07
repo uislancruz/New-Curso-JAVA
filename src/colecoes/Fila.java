95b7b57bdae0d023d8da3561be23ba92ee408842
Uislan Cruz <uislansk8@hotmail.com>
2022-02-07 19:28:24 -0300

    Update Fila.java

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -10,15 +10,15 @@ public class Fila {
 		//offer e add -> adicionam elementos na fila
 		// A diferença é o comportamento quando a fila está cheia!
 		
-		fila.add("Ana");
-		fila.offer("Bia");
-		fila.add("Carlos");
-		fila.offer("Daniel");
-		fila.add("Rafaela");
-		fila.offer("Gui");
+		/*
+		 * fila.add("Ana"); fila.offer("Bia"); fila.add("Carlos"); fila.offer("Daniel");
+		 * fila.add("Rafaela"); fila.offer("Gui");
+		 */
 
 		System.out.println(fila.peek());
 		System.out.println(fila.peek());
+		System.out.println(fila.element());
+		System.out.println(fila.element());
 		
 	}
 

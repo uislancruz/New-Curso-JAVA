2f88da87e437ac46f94fb4eda40bd7f7a28315c3
Uislan Cruz <uislansk8@hotmail.com>
2022-02-07 20:41:46 -0300

    Update Mapa.java

--- src/colecoes/Mapa.java
+++ src/colecoes/Mapa.java
@@ -8,8 +8,15 @@ public class Mapa {
 		
 		Map<Integer, String> usuarios = new HashMap<Integer, String>();
 		usuarios.put(1, "Roberto");
+		usuarios.put(2, "Ricardo");	
+		usuarios.put(3, "Rafaela");
+		usuarios.put(4, "Rebeca");	
 		
 		System.out.println(usuarios.size());
+		System.out.println(usuarios.isEmpty());
+		
+		
+		
 	}
 
 }

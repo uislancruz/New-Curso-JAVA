6b8810e0f60e1b11f18e0094a7f899c49777d210
Uislan Cruz <uislansk8@hotmail.com>
2022-02-07 19:26:22 -0300

    Update Fila.java

--- src/colecoes/Fila.java
+++ src/colecoes/Fila.java
@@ -7,8 +7,18 @@ public class Fila {
 	public static void main(String[] args) {
 		Queue<String> fila = new LinkedList<>();
 		
+		//offer e add -> adicionam elementos na fila
+		// A diferença é o comportamento quando a fila está cheia!
+		
 		fila.add("Ana");
 		fila.offer("Bia");
+		fila.add("Carlos");
+		fila.offer("Daniel");
+		fila.add("Rafaela");
+		fila.offer("Gui");
+
+		System.out.println(fila.peek());
+		System.out.println(fila.peek());
 		
 	}
 

70249f73324415414d6b77331f3214773b9bc599
Uislan Cruz <uislansk8@hotmail.com>
2022-02-07 20:07:58 -0300

    Update Pilha.java

--- src/colecoes/Pilha.java
+++ src/colecoes/Pilha.java
@@ -1,5 +1,17 @@
 package colecoes;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Pilha {
+	public static void main(String[] args) {
+		Deque<String> livros = new ArrayDeque<String>();
+		
+		livros.add("O Pequeno Principe");
+		livros.push("Don Quixote");
+		livros.push("O Hobbit");
+		
+		System.out.println(livros.peek());
+	}
 
 }

15a219b04368a73df1dcf287252b29df060a9542
Uislan Cruz <uislansk8@hotmail.com>
2022-02-08 20:45:48 -0300

    Estancia
    Estanciando um metodo.
    Aqui eu chamei o carro(c1), morto e giros.

--- src/oo/composicao/CarroTeste.java
+++ src/oo/composicao/CarroTeste.java
@@ -5,6 +5,12 @@ public class CarroTeste {
 	public static void main(String[] args) {
 		Carro c1 = new Carro();
 		System.out.println(c1.estaLigado());
+		
+		c1.ligar();
+		System.out.println(c1.estaLigado());
+		
+		System.out.println(c1.motor.giros());
+		
 	}
 
 }

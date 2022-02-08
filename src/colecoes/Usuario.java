051c6e3a9ea43c50b92f1ff536029f5f266ffb08
Uislan Cruz <uislansk8@hotmail.com>
2022-02-07 21:08:34 -0300

    Update Usuario.java

--- src/colecoes/Usuario.java
+++ src/colecoes/Usuario.java
@@ -15,11 +15,13 @@ public class Usuario {
 		
 	}
 
-	/*
-	 * @Override public int hashCode() { final int prime = 31; int result = 1;
-	 * result = prime * result + ((nome == null) ? 0 : nome.hashCode()); return
-	 * result; }
-	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
 
 	@Override
 	public boolean equals(Object obj) {

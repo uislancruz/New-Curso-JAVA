5cb2a15ad8b76176dfb869653c9b8e998468bb40
Uislan Cruz <uislansk8@hotmail.com>
2022-02-21 19:45:38 -0300

    Comentando

--- src/oo/composicao/Compra.java
+++ src/oo/composicao/Compra.java
@@ -1,5 +1,20 @@
 package oo.composicao;
 
+import java.util.ArrayList;
+
 public class Compra {
+	
+	String nomeCliente;
+	ArrayList<Item> itens = new ArrayList<Item>();
+	
+	double obterValorTotal() {
+		double total = 0;
+		
+		for(Item item: itens) {
+			total += item.quantidade * item.preco;
+		}
+		
+		return total;
+	}
 
 }

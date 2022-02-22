9eadb57896ef9886b9764afd42b25ed76fd243ec
Uislan Cruz <uislansk8@hotmail.com>
2022-02-21 21:03:29 -0300

    Update CursoTeste.java

--- src/oo/composicao/CursoTeste.java
+++ src/oo/composicao/CursoTeste.java
@@ -1,5 +1,35 @@
 package oo.composicao;
 
 public class CursoTeste {
+	
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno("João");
+		Aluno aluno2 = new Aluno("Maria");
+		Aluno aluno3 = new Aluno("Pedro");
+		
+		Curso curso1 = new Curso("Java Completo");
+		Curso curso2 = new Curso("Web 2023");
+		Curso curso3 = new Curso("React Native");
+		
+		
+		curso1.adicionarAluno(aluno1);
+		curso1.adicionarAluno(aluno2);
+		
+		curso2.adicionarAluno(aluno1);
+		curso2.adicionarAluno(aluno3);
+		
+		curso1.adicionarAluno(aluno3);
+		curso2.adicionarAluno(aluno3);
+		curso3.adicionarAluno(aluno3);
+		
+		for(Aluno aluno: curso1.alunos) {
+			
+			System.out.println("Estou matriculado no curso 1...");
+			System.out.println("...e o meu nome é " + aluno.nome );
+		}
+		
+		
+	}
 
 }

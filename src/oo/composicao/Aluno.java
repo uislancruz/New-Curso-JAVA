f86c4f9d85e90f658456980a0567271f94206b69
Uislan Cruz <uislansk8@hotmail.com>
2022-02-21 21:03:21 -0300

    Update Aluno.java

--- src/oo/composicao/Aluno.java
+++ src/oo/composicao/Aluno.java
@@ -1,5 +1,21 @@
 package oo.composicao;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class Aluno {
+	
+	final String nome;
+	final List<Curso> cursos = new ArrayList<>();
+	
+	Aluno(String nome){
+		this.nome = nome;
+	}
+	
+	void adicionarCurso(Curso curso) {
+		this.cursos.add(curso);
+		curso.alunos.add(this);
+	}
 
 }

30cf98d22b1e18c40e05973c9a01a7a5438bcaf8
Uislan Cruz <uislansk8@hotmail.com>
2022-03-01 19:49:20 -0300

    Update Compra.java

--- src/oo/composicao/desafio/Compra.java
+++ src/oo/composicao/desafio/Compra.java
@@ -1,5 +1,36 @@
 package oo.composicao.desafio;
 
-public class Compra {
+import java.util.List;
+import java.util.ArrayList;
 
+public class Compra {
+	
+	final List<Item> itens = new ArrayList<>();
+	
+	void adicionarItem(Produto p, int quantidade ) {
+		
+		this.itens.add(new Item(p, quantidade));
+		
+	}
+	
+	void adicionarItem(String nome, double preco, int qtde) {
+		var produto = new Produto(nome, preco);
+		this.itens.add(new Item(produto,qtde));
+	}
+	
+double obterValorTotal() {
+		
+		double total = 0;
+		
+		for(Item item: this.itens) {
+			
+			total += item.quantidade * item.produto.preco;
+			
+		}
+			
+		
+		
+		return total;
+
+}
 }

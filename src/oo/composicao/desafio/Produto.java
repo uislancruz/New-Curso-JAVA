b22abbf3af2a45ff52f0c90a32bd6812d6fdd8a7
Uislan Cruz <uislansk8@hotmail.com>
2022-03-01 19:49:12 -0300

    Update Produto.java

--- src/oo/composicao/desafio/Produto.java
+++ src/oo/composicao/desafio/Produto.java
@@ -1,5 +1,15 @@
 package oo.composicao.desafio;
 
 public class Produto {
+	
+	String nome;
+	double preco;
+	
+	Produto(String nome, double preco){
+		
+		this.nome = nome;
+		this.preco = preco;
+		
+	}
 
 }

3032ddc719b83d338123ac9179c676ab37541959
Uislan Cruz <uislansk8@hotmail.com>
2022-03-07 20:05:19 -0300

    Enum
    Implementando o enum na classe

--- src/oo/composicao/heranca/Jogador.java
+++ src/oo/composicao/heranca/Jogador.java
@@ -5,9 +5,22 @@ public class Jogador {
 	int x;
 	int y;
 	
-	boolean andar(String direcao) {
-		if("norte".equalsIgnoreCase(direcao)) {
+	boolean andar(Direcao direcao) {
+		
+		switch(direcao) {
+		
+		case NORTE:
+			y--;
+			break;
+		case LESTE:
+			x++;
+			break;
+		case SUL:
 			y++;
+			break;
+		case OESTE:
+			x--;
+			break;		
 		}
 		
 				

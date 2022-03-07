8816b37a29ed14be6bfc29441d87d0474f1992d0
Uislan Cruz <uislansk8@hotmail.com>
2022-03-07 20:05:28 -0300

    Enum

--- src/oo/composicao/heranca/Jogo.java
+++ src/oo/composicao/heranca/Jogo.java
@@ -9,12 +9,13 @@ public class Jogo {
 		j1.x = 10;
 		j1.y = 20;
 		
-		j1.andar("norte");
-		j1.andar("norte");
-		j1.andar("norte");
-		j1.andar("norte");
+		j1.andar(Direcao.NORTE);
+		j1.andar(Direcao.LESTE);
+		j1.andar(Direcao.NORTE);
+		j1.andar(Direcao.LESTE);
 		
 		System.out.println(j1.y);
+		System.out.println(j1.x);
 		
 		
 	}

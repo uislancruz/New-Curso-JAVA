608f10170105e9496a07d9df19e4b588a5057b07
Uislan Cruz <uislansk8@hotmail.com>
2022-03-07 19:53:21 -0300

    Update Jogador.java

--- src/oo/composicao/heranca/Jogador.java
+++ src/oo/composicao/heranca/Jogador.java
@@ -1,5 +1,17 @@
 package oo.composicao.heranca;
 
 public class Jogador {
+	
+	int x;
+	int y;
+	
+	boolean andar(String direcao) {
+		if("norte".equalsIgnoreCase(direcao)) {
+			y++;
+		}
+		
+				
+		return true;
+	}
 
 }

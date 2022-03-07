c9aaec4a365acb91f5e8f88dd394c26348e93b6d
Uislan Cruz <uislansk8@hotmail.com>
2022-03-07 20:17:07 -0300

    Update Jogo.java

--- src/oo/composicao/heranca/Jogo.java
+++ src/oo/composicao/heranca/Jogo.java
@@ -9,13 +9,19 @@ public class Jogo {
 		j1.x = 10;
 		j1.y = 10;
 		
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
+		Jogador j2 = new Jogador();
 		
-		System.out.println(j1.y);
-		System.out.println(j1.x);
+		j2.x = 10;
+		j2.y = 11;
+		
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
+		j1.atacar(j2);
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
 		
 		
 	}

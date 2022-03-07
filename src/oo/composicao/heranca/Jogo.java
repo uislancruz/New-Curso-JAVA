62940ba564fdf34d90a2aa5a9734dda778f0c652
Uislan Cruz <uislansk8@hotmail.com>
2022-03-07 19:53:17 -0300

    Update Jogo.java

--- src/oo/composicao/heranca/Jogo.java
+++ src/oo/composicao/heranca/Jogo.java
@@ -1,5 +1,17 @@
 package oo.composicao.heranca;
 
 public class Jogo {
-
+	
+	public static void main(String[] args) {
+		
+		Jogador j1 = new Jogador();
+		j1.andar("norte");
+		j1.andar("norte");
+		j1.andar("norte");
+		j1.andar("norte");
+		
+		System.out.println(j1.y);
+		
+		
+	}
 }

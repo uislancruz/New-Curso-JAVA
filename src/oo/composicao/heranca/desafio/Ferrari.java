4d9c375105e27c25d5eb1a3183869137b0eb0976
Uislan Cruz <uislansk8@hotmail.com>
2022-03-09 19:51:04 -0300

    Update Ferrari.java

--- src/oo/composicao/heranca/desafio/Ferrari.java
+++ src/oo/composicao/heranca/desafio/Ferrari.java
@@ -2,6 +2,16 @@ package oo.composicao.heranca.desafio;
 
 public class Ferrari extends Carro{
 	
+	Ferrari(){
+		
+		this(315);
+	}
+	
+	Ferrari(int velocidadeMaxima){
+		
+		super(velocidadeMaxima);
+	}
+	
 	@Override
 	void acelerar() {
 		

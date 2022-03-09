69dca273a395d5dcede3d0a2cd44d8600870b037
Uislan Cruz <uislansk8@hotmail.com>
2022-03-08 23:15:39 -0300

    Update Carro.java

--- src/oo/composicao/heranca/desafio/Carro.java
+++ src/oo/composicao/heranca/desafio/Carro.java
@@ -1,5 +1,35 @@
 package oo.composicao.heranca.desafio;
 
 public class Carro {
+	
+	int velocidadeAtual;
+	
+	
+	void acelerar() {
+		
+		velocidadeAtual += 5;
+		
+	}
+	
+	void frear() {
+		
+		if(velocidadeAtual >=5) {
+			
+		velocidadeAtual += 5;
+		
+		}else{
+			
+			velocidadeAtual = 0;
+		}
+		
+				
+	}
+	
+	
+	public String toString() {
+		
+		return "Velocidade atual é: " + velocidadeAtual + " Km/hr";
+		
+	}
 
 }

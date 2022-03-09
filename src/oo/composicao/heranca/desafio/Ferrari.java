94b6634b5d4f5951dd2371cbdc9166a71f525343
Uislan Cruz <uislansk8@hotmail.com>
2022-03-09 19:59:05 -0300

    Update Ferrari.java

--- src/oo/composicao/heranca/desafio/Ferrari.java
+++ src/oo/composicao/heranca/desafio/Ferrari.java
@@ -10,12 +10,15 @@ public class Ferrari extends Carro{
 	Ferrari(int velocidadeMaxima){
 		
 		super(velocidadeMaxima);
+		delta = 15;
 	}
 	
-	@Override
-	void acelerar() {
-		
-		velocidadeAtual += 15;
-	}
+	//com a declaração do delta dentro do contrutor, se faz desnecessario a parte de baixo
+	
+//	@Override
+//	void acelerar() {
+//		
+//		velocidadeAtual += 15;
+//	}
 
 }

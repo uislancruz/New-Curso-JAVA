ed778fe9632ee3a37f2ba73d7b6293acd87ca09c
Uislan Cruz <uislansk8@hotmail.com>
2022-03-11 20:13:47 -0300

    Update Ana.java

--- src/oo/encapsulamento/casaA/Ana.java
+++ src/oo/encapsulamento/casaA/Ana.java
@@ -1,5 +1,11 @@
 package oo.encapsulamento.casaA;
 
 public class Ana {
-
+	
+	@SuppressWarnings("unused")//para tirar o amarelinho da variavel segredo
+	private String segredo ="...";
+	String facoDentrodeCasa="..."; //default ou pacote
+	protected String formaDefalar = "...";
+	public String todosSabem = "...";
+	
 }

e60316774367607981817b16a1d5b6d2f5fcaee5
Uislan Cruz <uislansk8@hotmail.com>
2022-03-11 20:13:51 -0300

    Update Paulo.java

--- src/oo/encapsulamento/casaA/Paulo.java
+++ src/oo/encapsulamento/casaA/Paulo.java
@@ -1,5 +1,28 @@
 package oo.encapsulamento.casaA;
 
+
 public class Paulo {
+	
+	
+	void testeAcessos() {
+
+		Ana esposa = new Ana();
+		
+//		segredo
+//		facoDentrodeCasa
+//		formaDefalar
+//		todosSabem
+		
+		
+		System.out.println(esposa.segredo);
+		System.out.println(esposa.facoDentrodeCasa);
+		System.out.println(esposa.formaDefalar);
+		System.out.println(esposa.todosSabem);
+		
+		
+		
+	}
+	
+	
 
 }

6e3f309ad9019e5e1514550e0c830ed6c741d4d0
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 12:31:09 -0300

    Create Jogo.java

---
+++ src/oo/composicao/heranca/teste/Jogo.java
@@ -0,0 +1,41 @@
+package oo.composicao.heranca.teste;
+
+import oo.composicao.heranca.desafio.Direcao;
+import oo.composicao.heranca.desafio.Heroi;
+import oo.composicao.heranca.desafio.Monstro;
+
+public class Jogo {
+	
+	public static void main(String[] args) {
+		
+		Monstro monstro = new Monstro();
+		
+		monstro.x = 10;
+		monstro.y = 10;
+		
+		Heroi heroi = new Heroi();
+		
+		heroi.x = 10;
+		heroi.y = 11;	
+		
+		
+		System.out.println("Monstro tem =>" + monstro.vida);
+		System.out.println("Monstro tem =>" +heroi.vida);
+		
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		monstro.andar(Direcao.NORTE);
+		monstro.atacar(heroi);
+		heroi.atacar(monstro);
+		
+		System.out.println("Monstro tem =>" + monstro.vida);
+		System.out.println("Monstro tem =>" +heroi.vida);
+		
+		
+	}
+}

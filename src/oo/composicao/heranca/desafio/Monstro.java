8451bdec65205ea021f93245396eb93fc9600963
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 12:51:14 -0300

    Update Monstro.java

--- src/oo/composicao/heranca/desafio/Monstro.java
+++ src/oo/composicao/heranca/desafio/Monstro.java
@@ -1,5 +1,14 @@
 package oo.composicao.heranca.desafio;
 
 public class Monstro extends Jogador{
+	
+	public Monstro() {
+		super();
+	}
+	
+	public Monstro(int x, int y){
+		
+		super(x, y);
+	}
 
 }

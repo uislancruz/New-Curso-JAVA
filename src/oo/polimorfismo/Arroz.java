3148162396a519308947b0e1dd4dde4d5fc71f51
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 17:03:30 -0300

    Continuação Polimorfismo

--- src/oo/polimorfismo/Arroz.java
+++ src/oo/polimorfismo/Arroz.java
@@ -1,5 +1,21 @@
 package oo.polimorfismo;
 
 public class Arroz {
+	
+	private double peso;
+	
+	public Arroz(double peso) {
+		setPeso(peso);
+	}
+	
+	public double getPeso(double peso) {
+		return peso;
+	}
+	
+	public void setPeso(double peso) {
+		if(peso >= 0) {
+			this.peso = peso;
+		}
+	}
 
 }

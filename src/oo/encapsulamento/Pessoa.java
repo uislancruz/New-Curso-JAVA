394f2c532bed1923c794474eaa7951dc12e4c464
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 13:34:26 -0300

    Update Pessoa.java

--- src/oo/encapsulamento/Pessoa.java
+++ src/oo/encapsulamento/Pessoa.java
@@ -2,7 +2,21 @@ package oo.encapsulamento;
 
 public class Pessoa {
 	
-	public int idade;
+	private int idade;
+	
+	public Pessoa(int idade){
+		this.idade = idade;
 		
+	}
+	
+	public int lerIdade() {
+		
+		return idade;
+	}
+		
+	public void alterarIdade(int novaIdade) {
+		this.idade = novaIdade;
+		
+	}
 
 }

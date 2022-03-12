03afc6a19888d7e287625b9e215171a89269da2b
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 13:48:19 -0300

    Getters & Setters
    Implementando os Getters e setters

--- src/oo/encapsulamento/Pessoa.java
+++ src/oo/encapsulamento/Pessoa.java
@@ -3,20 +3,41 @@ package oo.encapsulamento;
 public class Pessoa {
 	
 	private int idade;
+	private String nome;
 	
-	public Pessoa(int idade){
-		this.idade = idade;
+	public Pessoa(String nome, int idade){
+		setNome(nome);
+		setIdade(idade);
 		
 	}
 	
-	public int lerIdade() {
+	
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+
+	//Getter
+	public int getIdade() {
 		
 		return idade;
 	}
-		
-	public void alterarIdade(int novaIdade) {
+		//Setter
+	public void setIdade(int novaIdade) {
+		novaIdade = Math.abs(novaIdade);
+		if(novaIdade >= 0 && novaIdade <= 120) {
 		this.idade = novaIdade;
 		
+		}
 	}
 
 }

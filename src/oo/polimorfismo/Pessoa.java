7b60a0171ecc13afa2fdf71c3931591e93df518b
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 17:24:22 -0300

    Update Pessoa.java

--- src/oo/polimorfismo/Pessoa.java
+++ src/oo/polimorfismo/Pessoa.java
@@ -1,5 +1,25 @@
 package oo.polimorfismo;
 
 public class Pessoa {
+	
+ private double peso;
+	
+	public Pessoa (double peso) {
+		setPeso(peso);
+	}
+	
+	public void comer(Arroz arroz) {
+		this.peso += arroz.getPeso(peso);
+	}
+	
+	public double getPeso(double peso) {
+		return peso;
+	}
+	
+	public void setPeso(double peso) {
+		if(peso >= 0) {
+			this.peso = peso;
+		}
+	}
 
 }

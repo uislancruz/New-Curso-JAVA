85b5b753a94602960838ecdf54635193c2c38e49
Uislan Cruz <uislansk8@hotmail.com>
2022-03-12 16:28:43 -0300

    Ajustes

--- src/oo/encapsulamento/Pessoa.java
+++ src/oo/encapsulamento/Pessoa.java
@@ -3,13 +3,28 @@ package oo.encapsulamento;
 public class Pessoa {
 	
 	private int idade;
+	private String sobreNome;
 	private String nome;
 	
-	public Pessoa(String nome, int idade){
+	public Pessoa(String nome,String sobreNome, int idade){
+		
+		setSobrenome(sobreNome);
 		setNome(nome);
 		setIdade(idade);
 		
 	}
+	
+	
+	public String getSobrenome() {
+		return sobreNome;
+	}
+
+
+	public void setSobrenome(String sobrenome) {
+		this.sobreNome = sobrenome;
+	}
+
+
 	//Getter
 	public String getNome() {
 		return nome;
@@ -17,6 +32,11 @@ public class Pessoa {
 	//Setter
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public String getDadosCompletos() {
+		
+		return getNome() + " " + getSobrenome() + " " + getIdade();
 	}
 
 
@@ -37,7 +57,7 @@ public class Pessoa {
 	@Override
 	public String toString() {
 		
-		return "Ola, eu sou o " + getNome() + " e tenho "+ getIdade();
+		return "Ola, eu sou o " + getNome() + getSobrenome() + " e tenho "+ getIdade();
 	}
 
 }
--- src/oo/encapsulamento/PessoaTeste.java
+++ src/oo/encapsulamento/PessoaTeste.java
@@ -3,11 +3,12 @@ package oo.encapsulamento;
 public class PessoaTeste {
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa("Pedro", -30);
+		Pessoa p1 = new Pessoa("Pedro", " Amoedo", -30);
 		p1.setNome("Pedro");	
 		p1.setIdade(30);	
 		
 		System.out.println(p1.getNome()+"\n" +p1.getIdade());//ler normal
 		System.out.println(p1);//quando vc denomina o ToString(muito melhor)
+		System.out.println(p1.getDadosCompletos());
 }
 }

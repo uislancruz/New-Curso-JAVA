ea873c1828a075399a2cf7ca6431919439722f1f
Uislan Cruz <uislansk8@hotmail.com>
2022-03-13 14:46:56 -0300

    Update CalculoTeste2.java

--- src/lambdas/CalculoTeste2.java
+++ src/lambdas/CalculoTeste2.java
@@ -4,9 +4,11 @@ public class CalculoTeste2 {
 	
 	public static void main(String[] args) {
 		
-		Calculo calc = (x, y) -> {return x + y; };
+		Calculo calc = (x, y) -> {return x + y; }; //Lambda
 		
 		System.err.println(calc.executar(2, 3));
+		
+		calc = (x, y) -> x * y;//Lambda pode ser assim omitindo o return sem {return}
 		
 	}
 

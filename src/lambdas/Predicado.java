b28e746e41bc0ef8172ea9ca9ebe8d00a0381e32
Uislan Cruz <uislansk8@hotmail.com>
2022-03-14 20:55:38 -0300

    Update Predicado.java

--- src/lambdas/Predicado.java
+++ src/lambdas/Predicado.java
@@ -1,5 +1,20 @@
 package lambdas;
 
-public class Predicado {
+import java.util.function.Predicate;
 
+public class Predicado {
+	
+	public static void main(String[] args) {
+		
+
+	Predicate<Produto> isCaro = produtoVariavel -> 
+			(produtoVariavel.preco * (1 - produtoVariavel.desconto)) >= 750;
+	
+	
+	Produto produto = new Produto("Notebook", 3893.89, 0.15);
+	
+	System.out.println(isCaro.test(produto));
+	
+
+}
 }

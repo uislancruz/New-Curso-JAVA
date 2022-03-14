49b99ee8e41f04c6f37542b9192c3538da52153d
Uislan Cruz <uislansk8@hotmail.com>
2022-03-14 19:17:34 -0300

    Create CalculoTeste3.java

---
+++ src/lambdas/CalculoTeste3.java
@@ -0,0 +1,19 @@
+package lambdas;
+
+public class CalculoTeste3 {
+	
+	public static void main(String[] args) {
+		
+		Calculo calc = (x, y) -> {return x + y; }; //Lambda
+		
+		System.err.println(calc.executar(2, 3));
+		
+		calc = (x, y) -> x * y;//Lambda pode ser assim omitindo o return sem {return}
+		
+		System.out.println(calc.executar(2, 3));
+		System.out.println(calc.legal());
+		System.out.println(Calculo.muitoLegal());//Acessando o metodo statico
+		
+	}
+
+}

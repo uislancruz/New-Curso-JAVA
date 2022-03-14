40b258d81d79497989656793d8e539e637de082c
Uislan Cruz <uislansk8@hotmail.com>
2022-03-14 20:05:52 -0300

    Update Foreach.java

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -4,6 +4,7 @@ package lambdas;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Foreach {
 	
 	public static void main(String[] args) {
@@ -15,6 +16,15 @@ public class Foreach {
 		for(String nome: aprovados) {
 			System.out.println(nome);
 		}
+		
+		
+		System.out.println("\nLambda #01...");
+		
+		aprovados.forEach((nome)-> {System.out.println(nome+ "!!!");});
+		
+		System.out.println("\nMethod Reference...");
+		
+		aprovados.forEach(System.out::println);
 		
 	}
 

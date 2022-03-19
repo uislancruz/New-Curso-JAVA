c33d6b969f30467d356e583a5e2921efbb7e06ed
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 14:35:23 -0300

    Update Funcao.java

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -7,11 +7,9 @@ public class Funcao {
 	public static void main(String[] args) {
 		
 		Function<Integer, String> parOuImpar = numero -> numero % 2 == 0? "Par" : "Impar";
-		
-				
+						
 		Function<String, String> oResultadoE = valor -> "O resultado é: " + valor;
-		
-		
+				
 		Function<String, String> empolgado = valor -> valor + "!!!";
 		
 		Function<String, String> duvida = valor -> valor + "???";

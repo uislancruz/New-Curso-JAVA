b8d0fb6699b09098e849b8399f166defd7b37f91
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 14:38:15 -0300

    Update PredicadoComposicao.java

--- src/lambdas/PredicadoComposicao.java
+++ src/lambdas/PredicadoComposicao.java
@@ -11,7 +11,8 @@ public class PredicadoComposicao {
 		Predicate<Integer> isPar = num -> num %2 == 0;
 		Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
 		
-		System.out.println(isPar.test(22));
+		System.out.println(isPar.and(isTresDigitos).negate().test(123));
+		System.out.println(isPar.or(isTresDigitos).test(123));
 		
 	}
 

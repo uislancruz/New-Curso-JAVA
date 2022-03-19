37f636c1cb8e323cc460e131fa5636bb696ab842
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 16:45:37 -0300

    Update OperadoraUnario.java

--- src/lambdas/OperadoraUnario.java
+++ src/lambdas/OperadoraUnario.java
@@ -10,14 +10,21 @@ public class OperadoraUnario {
 		UnaryOperator<Integer> vezesDois = n -> n *2;
 		UnaryOperator<Integer> aoQuadrado = n -> n * n;
 		
-		int resultado1 = maisDois
-				.andThen(vezesDois)
-				.andThen(aoQuadrado)
+		int resultado1 = maisDois // excecute essa
+				.andThen(vezesDois)// depois excecute essa
+				.andThen(aoQuadrado)// e depois essa
 				.apply(0);
 		
 		//anThen é para fazer composição de função
 		
 		System.out.println(resultado1);
+		
+		int resultado2 = aoQuadrado
+				.compose(vezesDois)
+				.compose(maisDois)
+				.apply(0);
+		
+		System.out.println(resultado2);
 		
 	}
 

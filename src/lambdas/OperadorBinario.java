7c381ca9c79764f3f89e80e2fac9f966731ca079
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 19:35:46 -0300

    Mais "Organizado"

--- src/lambdas/OperadorBinario.java
+++ src/lambdas/OperadorBinario.java
@@ -11,8 +11,9 @@ public class OperadorBinario {
 		
 		System.out.println(media.apply(9.8, 5.7));
 		
-		BiFunction<Double, Double, String> resultado =
-				(n1, n2) -> ((n1 + n2) /2) >= 7? "Aprovado": "Reprovado";
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+					Double notaFinal = (n1 + n2) /2;
+					return notaFinal >= 7? "Aprovado": "Reprovado";};
 				
 				System.out.println(resultado.apply(9.7, 5.1));
 		

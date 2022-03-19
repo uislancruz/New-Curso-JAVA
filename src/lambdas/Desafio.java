6d00378cb5217bd47c2b6cc8ec257341bcd53df1
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 20:47:15 -0300

    Update Desafio.java

--- src/lambdas/Desafio.java
+++ src/lambdas/Desafio.java
@@ -1,5 +1,34 @@
 package lambdas;
 
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 public class Desafio {
+	
+	public static void main(String[] args) {
+		
+		Produto p = new Produto("iPad", 3235.89, 0.13);
+		
+		Function<Produto, Double> precoFinal = 
+				produtoVariavel -> produtoVariavel.preco *(1- produtoVariavel.desconto);
+		UnaryOperator<Double> impostoMunicipal = 
+				preco -> preco >= 2500 ? preco * 1.085 : preco;
+		UnaryOperator<Double> frete =
+				preco -> preco >= 3000 ? preco + 100 : preco;
+		UnaryOperator<Double> arredondar = 
+				preco -> Double.parseDouble(String.format("%.2f", preco));
+		Function<Double, String> formatar = 
+				preco -> ("R$" + preco).replace(".", ",");
+		String preco = precoFinal
+				.andThen(impostoMunicipal)
+				.andThen(frete)
+				.andThen(arredondar)
+				.andThen(formatar)
+				.apply(p);
+		
+		System.out.println("O preço final é " + preco);
+		
+		
+	}
 
 }

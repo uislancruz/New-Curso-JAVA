f3c101e1248b3b5c0ad55c5fadd6fbcee92c65f0
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 14:54:40 -0300

    Update Fornecedor.java

--- src/lambdas/Fornecedor.java
+++ src/lambdas/Fornecedor.java
@@ -10,6 +10,8 @@ public class Fornecedor {
 		
 		Supplier<List<String>> umLista = () -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
 		
+		//por não receber nenhum parametro, colocamos os ().
+		
 		System.out.println(umLista.get());
 	}
 

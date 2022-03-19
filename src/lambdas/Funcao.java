8a1283a03c56702a692f352f90092a31d04774a8
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 13:50:37 -0300

    Update Funcao.java

--- src/lambdas/Funcao.java
+++ src/lambdas/Funcao.java
@@ -9,6 +9,9 @@ public class Funcao {
 		Function<Integer, String> parOuImpar = numero -> numero % 2 == 0? "Par" : "Impar";
 		
 		System.out.println(parOuImpar.apply(32));
+		
+		Function<String, String> oResultadoE = valor -> "O resultado é: " + valor;
+		System.out.println(oResultadoE.apply("Teste"));
 	}
 
 }

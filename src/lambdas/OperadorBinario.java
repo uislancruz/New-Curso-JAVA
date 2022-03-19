c4b43b07e7060804a16e6169474fb2511b1c0c78
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 19:54:19 -0300

    Update OperadorBinario.java

--- src/lambdas/OperadorBinario.java
+++ src/lambdas/OperadorBinario.java
@@ -2,6 +2,7 @@ package lambdas;
 
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class OperadorBinario {
 	
@@ -12,11 +13,15 @@ public class OperadorBinario {
 		System.out.println(media.apply(9.8, 5.7));
 		
 		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
-					Double notaFinal = (n1 + n2) /2;
+					double notaFinal = (n1 + n2) /2;
 					return notaFinal >= 7? "Aprovado": "Reprovado";};
 				
 				System.out.println(resultado.apply(9.7, 5.1));
 		
+		Function<Double, String> conceito = 
+				m -> m >= 7 ? "Aprovado" : "Reprovado";
+				
+				System.out.println(media.andThen(conceito).apply(9.7, 4.1));
 		
 	}
 

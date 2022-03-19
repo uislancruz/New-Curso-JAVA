3203791a09799fe8456012f304f5b017e51fb028
Uislan Cruz <uislansk8@hotmail.com>
2022-03-19 12:31:54 -0300

    Update Consumidor.java

--- src/lambdas/Consumidor.java
+++ src/lambdas/Consumidor.java
@@ -1,5 +1,14 @@
 package lambdas;
 
-public class Consumidor {
+import java.util.function.Consumer;
 
+public class Consumidor {
+	public static void main(String[] args) {
+		
+		Consumer<Produto> imprimir = p -> System.out.println(p.nome);
+		
+		Produto p1 = new Produto("Caneta", 12.34, 0.09);
+		
+		imprimir.accept(p1);
+	}
 }

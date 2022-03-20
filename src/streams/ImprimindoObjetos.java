c2a9609bbff5d37dbab8011621220883f47e192a
Uislan Cruz <uislansk8@hotmail.com>
2022-03-20 18:57:48 -0300

    Update ImprimindoObjetos.java

--- src/streams/ImprimindoObjetos.java
+++ src/streams/ImprimindoObjetos.java
@@ -1,5 +1,18 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ImprimindoObjetos {
+	
+	public static void main(String[] args) {
+		
+		List<String> aprovados = Arrays.asList("Lu", "Gui", "Lucas", "Ana");
+		
+		System.out.println(aprovados);
+		
+		
+		
+	}
 
 }

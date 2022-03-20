276cd2d7cf1082abf67d2e0e06c8aab824fb11d5
Uislan Cruz <uislansk8@hotmail.com>
2022-03-20 14:38:16 -0300

    Thread

--- src/lambdas/Trabalho1.java
+++ src/lambdas/Trabalho1.java
@@ -1,5 +1,12 @@
 package lambdas;
 
-public class Trabalho1 {
+public class Trabalho1 implements Runnable{
+	
+	public void run() {
+		
+		for(int i = 0; i < 100; i ++) {
+			System.out.println("Tarefa #01");
+		}
+	}
 
 }

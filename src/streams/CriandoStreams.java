09e6fe89f5bbfdfe81917f0a6827a4088834fd56
Uislan Cruz <uislansk8@hotmail.com>
2022-03-21 19:37:44 -0300

    Update CriandoStreams.java

--- src/streams/CriandoStreams.java
+++ src/streams/CriandoStreams.java
@@ -1,5 +1,18 @@
 package streams;
 
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 public class CriandoStreams {
+	
+	public static void main(String[] args) {
+		
+		Consumer<String> print = System.out::print;
+		
+		Stream<String> langs = Stream.of("Java", " Lua", " JS\n");
+		
+		langs.forEach(print);
+		
+	}
 
 }

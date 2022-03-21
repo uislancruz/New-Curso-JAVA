fed7469cdd96a0d3c4de449fc7fc63d6ecd8018b
Uislan Cruz <uislansk8@hotmail.com>
2022-03-21 20:35:30 -0300

    Update Map.java

--- src/streams/Map.java
+++ src/streams/Map.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 public class Map {
 	
@@ -12,6 +13,13 @@ public class Map {
 		List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda ");
 		
 		marcas.stream().map(m -> m.toUpperCase()).forEach(print);
+		
+		UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+		UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + " ";
+		UnaryOperator<String> grito = n -> n + "!!!";
+		
+		maiuscula.andThen(primeiraLetra).andThen(grito).apply("BMW");
+		
 	}
 
 }

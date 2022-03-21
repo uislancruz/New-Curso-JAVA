0b8e4b427f6d5be9ddf399a128d1602ddb5272df
Uislan Cruz <uislansk8@hotmail.com>
2022-03-21 19:08:15 -0300

    Update ImprimindoObjetos.java

--- src/streams/ImprimindoObjetos.java
+++ src/streams/ImprimindoObjetos.java
@@ -1,6 +1,7 @@
 package streams;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class ImprimindoObjetos {
@@ -9,11 +10,21 @@ public class ImprimindoObjetos {
 		
 		List<String> aprovados = Arrays.asList("Lu", "Gui", "Lucas", "Ana");
 			System.out.println(aprovados);
+			
 			System.out.println(" ");
+			
 			System.out.println("Usando o foreach...");
 			for(String nome: aprovados) {
 				System.out.println(nome);
 			}
+			
+			System.out.println("\nUsando Iterator...");
+			
+			Iterator<String> it = aprovados.iterator();
+			while(it.hasNext()) {
+				System.out.println(it.next());
+			}
+			
 		
 	}
 

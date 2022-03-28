7c1db449b8436db89c7a0de5d9d3c191edc2f1e2
Uislan Cruz <uislansk8@hotmail.com>
2022-03-28 19:11:28 -0300

    Update DesafioMap.java

--- src/streams/DesafioMap.java
+++ src/streams/DesafioMap.java
@@ -1,5 +1,14 @@
 package streams;
 
-public class DesafioMap {
+import java.util.Arrays;
+import java.util.List;
 
+public class DesafioMap {
+	
+	
+	List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	
+	
+	
+	
 }

2f5d1ae7b8efc0a69f716f49acba509f49e78b77
Uislan Cruz <uislansk8@hotmail.com>
2022-03-28 22:37:35 -0300

    Filter

--- src/streams/Aluno.java
+++ src/streams/Aluno.java
@@ -1,5 +1,17 @@
 package streams;
 
 public class Aluno {
+	
+	final String nome;
+	final double nota;
+	
+	
+	public Aluno(String nome, double nota) {
+		
+		this.nome = nome;
+		this.nota = nota;
+	}
+	
+	
 
 }

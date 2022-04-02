96f78313de0f83102a71f5b9efb0dafe57ad1e55
Uislan Cruz <uislansk8@hotmail.com>
2022-04-02 12:37:35 -0300

    Update Reduce1.java

--- src/streams/Reduce1.java
+++ src/streams/Reduce1.java
@@ -1,5 +1,21 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
 public class Reduce1 {
+	
+	public static void main(String[] args) {
+		
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		BinaryOperator<Integer> soma = (acumuladorVariavel, nVariavel)
+				-> acumuladorVariavel + nVariavel;
+		
+		int total = nums.stream().reduce(soma).get();
+		System.out.println(total);
+		
+	}
 
 }

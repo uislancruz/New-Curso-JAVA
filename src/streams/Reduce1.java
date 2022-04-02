6ca861be4c36080f7b8d80d6435c5c56501214f6
Uislan Cruz <uislansk8@hotmail.com>
2022-04-02 13:16:53 -0300

    Update Reduce1.java

--- src/streams/Reduce1.java
+++ src/streams/Reduce1.java
@@ -18,9 +18,14 @@ public class Reduce1 {
 		
 		//Integer total2 = nums.parallelStream().reduce(100, soma);//com o parallelStream ele coloca o valor inicial em cada chamada. Na lista tem 9 elemento, então ele colocou o 100 nove vezes.
 		Integer total2 = nums.stream().reduce(100, soma);//com stream apenas, ele colocar o valor inicial apenas na primeira chamada.
-		
-		
 		System.out.println(total2);
+		
+		
+		//Resultado foi um Opcional<Integer>...
+		nums.stream()
+		.filter(nVariavel2 -> nVariavel2 > 5)
+		.reduce(soma)
+		.ifPresent(System.out::println);
 		
 	}
 

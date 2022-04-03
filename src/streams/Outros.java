85595ee395d7ed986750d44f8ffd319ae4fbf54c
Uislan Cruz <uislansk8@hotmail.com>
2022-04-03 13:49:14 -0300

    Update Outros.java

--- src/streams/Outros.java
+++ src/streams/Outros.java
@@ -12,15 +12,29 @@ public class Outros {
 		Aluno a3 = new Aluno("Gui", 8.1);
 		Aluno a4 = new Aluno("Gabi", 10);
 		Aluno a5 = new Aluno("Ana", 7.1);
-		Aluno a6 = new Aluno("Luna", 6.1);
+		Aluno a6 = new Aluno("Pedro", 6.1);
 		Aluno a7 = new Aluno("Gui", 8.1);
-		Aluno a8 = new Aluno("Gabi", 10);
+		Aluno a8 = new Aluno("Maria", 10);
 		
 		List<Aluno> aluno = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8);
 		
 		System.out.println("distinct...");
 		aluno.stream().distinct().forEach(System.out::println);
 		
+		System.out.println("\nSkip/Limit");
+		
+		aluno.stream()
+		.distinct()
+		.skip(2)
+		.limit(2)
+		.forEach(System.out::println);
+		
+		
+		System.out.println("\ntakeWhile");
+		aluno.stream()
+		.distinct()
+		.takeWhile(a -> a.nota >= 7)
+		.forEach(System.out::println);
 		
 	}
 	

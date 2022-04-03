0774ebe5fc6b9a2561126b9497c02737a3cbb409
Uislan Cruz <uislansk8@hotmail.com>
2022-04-03 13:34:54 -0300

    Ajustes

--- src/streams/Outros.java
+++ src/streams/Outros.java
@@ -11,9 +11,9 @@ public class Outros {
 		Aluno a2 = new Aluno("Luna", 6.1);
 		Aluno a3 = new Aluno("Gui", 8.1);
 		Aluno a4 = new Aluno("Gabi", 10);
-		Aluno a5 = new Aluno("Ana", 10);
-		Aluno a6 = new Aluno("Luna", 10);
-		Aluno a7 = new Aluno("Gui", 10);
+		Aluno a5 = new Aluno("Ana", 7.1);
+		Aluno a6 = new Aluno("Luna", 6.1);
+		Aluno a7 = new Aluno("Gui", 8.1);
 		Aluno a8 = new Aluno("Gabi", 10);
 		
 		List<Aluno> aluno = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8);

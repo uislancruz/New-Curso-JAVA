8ecf36c67a5e87543ec7c6b5f688a5156f528b5a
Uislan Cruz <uislansk8@hotmail.com>
2022-04-11 20:26:16 -0300

    Tratativa de erros

--- src/excecao/ChecadaVsNaoChecada.java
+++ src/excecao/ChecadaVsNaoChecada.java
@@ -1,5 +1,25 @@
 package excecao;
 
+import javax.management.RuntimeErrorException;
+
 public class ChecadaVsNaoChecada {
+	public static void main(String[] args) {
+		geraErro1();
+		geraErro2();
+		
+		System.out.println("FIM :)");
+				
+	}
+	
+	static void geraErro1() {
+		
+		new RuntimeException("Ocorreu um erro bem legal #01!");
+		
+	}
+	
+	static void geraErro2() {
+		new Exception("Ocorreu um erro bem legal #02!");
+		
+	}
 
 }

b8f708474f5029033c5460346ee0425ae6e031ef
Uislan Cruz <uislansk8@hotmail.com>
2022-04-11 20:39:58 -0300

    Update ChecadaVsNaoChecada.java

--- src/excecao/ChecadaVsNaoChecada.java
+++ src/excecao/ChecadaVsNaoChecada.java
@@ -4,15 +4,19 @@ import javax.management.RuntimeErrorException;
 
 public class ChecadaVsNaoChecada {
 	public static void main(String[] args) {
-		geraErro1();
+		try {
+			geraErro1();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} 
+		
 		geraErro2();
 		
 		System.out.println("FIM :)");
 				
 	}
-	
+	//Não checada ou não verificada
 	static void geraErro1() {
-		
 		throw new RuntimeException("Ocorreu um erro bem legal #01!");
 		
 	}

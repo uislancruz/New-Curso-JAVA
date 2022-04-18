0a123da0630a3afba120a71573282c78087ca463
Uislan Cruz <uislansk8@hotmail.com>
2022-04-17 23:29:18 -0300

    Update TesteValidacoes.java

--- src/excecao/personalizadaA/TesteValidacoes.java
+++ src/excecao/personalizadaA/TesteValidacoes.java
@@ -7,9 +7,13 @@ public class TesteValidacoes {
 	public static void main(String[] args) {
 		
 		try {
-			Aluno aluno = new Aluno("", 7);
+			Aluno aluno = new Aluno("Ana", 7);
 			Validar.aluno(aluno);
+			
+			Validar.aluno(null);
 		} catch (StringVaziaException e) {
+			System.out.println(e.getMessage());
+		}catch(NumeroForaIntervaloException | IllegalArgumentException e) {
 			System.out.println(e.getMessage());
 		}
 		

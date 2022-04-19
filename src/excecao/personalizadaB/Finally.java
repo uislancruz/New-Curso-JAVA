caa0547bdd480b0c89d1c12dbc5455279b8eedb8
Uislan Cruz <uislansk8@hotmail.com>
2022-04-18 23:18:00 -0300

    Update Finally.java

--- src/excecao/personalizadaB/Finally.java
+++ src/excecao/personalizadaB/Finally.java
@@ -1,5 +1,22 @@
 package excecao.personalizadaB;
 
+import java.util.Scanner;
+
 public class Finally {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			Scanner entrada = new Scanner(System.in);
+			
+			System.out.println(7 / entrada.nextInt());
+			
+		}catch(Exception e) {
+			
+		}
+		
+		System.out.println("FIM!");
+	}
 
 }

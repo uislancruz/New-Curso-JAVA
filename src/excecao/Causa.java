11408c2267e968fbf1bfe280b5cd9cb08ee16906
Uislan Cruz <uislansk8@hotmail.com>
2022-04-18 23:40:08 -0300

    Update Causa.java

--- src/excecao/Causa.java
+++ src/excecao/Causa.java
@@ -3,14 +3,18 @@ package excecao;
 public class Causa {
 	
 	public static void main(String[] args) {
-		
+		metodoA(null);
 	}
 	
 	static void metodoA(Aluno aluno) {
-		
+		metodoB(aluno);
 	}
 	
     static void metodoB(Aluno aluno) {
-		
+    	if(aluno == null) {
+    		throw new NullPointerException("Aluno está Nulo");
+    	}
+    	
+    	System.out.println(aluno.nome);
 	}
 }

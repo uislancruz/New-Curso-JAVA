2dfda28bec8608c06a49b7c6fdd8b12db680cacf
Uislan Cruz <uislansk8@hotmail.com>
2022-04-18 23:39:15 -0300

    Create Causa.java

---
+++ src/excecao/Causa.java
@@ -0,0 +1,16 @@
+package excecao;
+
+public class Causa {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	static void metodoA(Aluno aluno) {
+		
+	}
+	
+    static void metodoB(Aluno aluno) {
+		
+	}
+}

593a298079514baa68ffbf59a51e0ade456fd823
Uislan Cruz <uislansk8@hotmail.com>
2022-04-26 19:39:11 -0300

    Update ListaUtilTeste.java

--- src/generics/ListaUtilTeste.java
+++ src/generics/ListaUtilTeste.java
@@ -9,11 +9,17 @@ public class ListaUtilTeste {
 		List<String> langs = Arrays.asList("JS", "PHP", "Java", "C++");
 		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6);
 		
-		String ultimaLinguagem = (String) ListaUtil.getUltimo1(langs);
-		System.out.println(ultimaLinguagem);
+		String ultimaLinguagem1 = (String) ListaUtil.getUltimo1(langs);
+		System.out.println(ultimaLinguagem1);
 		
-		Integer ultimoNumero = (Integer) ListaUtil.getUltimo1(nums);
-		System.out.println(ultimoNumero);
+		Integer ultimoNumero1 = (Integer) ListaUtil.getUltimo1(nums);
+		System.out.println(ultimoNumero1);
+		
+		String ultimaLinguagem2 = (String) ListaUtil.getUltimo1(langs);
+		System.out.println(ultimaLinguagem2);
+		
+		Integer ultimoNumero2 = (Integer) ListaUtil.getUltimo1(nums);
+		System.out.println(ultimoNumero2);
 	}
 
 }

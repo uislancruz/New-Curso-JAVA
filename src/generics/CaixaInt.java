020cd062169d10dbd7af32681b350d669127fcc0
Uislan Cruz <uislansk8@hotmail.com>
2022-04-26 18:58:35 -0300

    Testando com Inteiro
    Dizendo que a classe deixou de ser generico para indicar um certo tipo, no caso o INT

--- src/generics/CaixaInt.java
+++ src/generics/CaixaInt.java
@@ -1,5 +1,5 @@
 package generics;
 
-public class CaixaInt<X> extends Caixa<X>{
+public class CaixaInt extends Caixa<Integer>{
 
 }
--- src/generics/CaixaIntTeste.java
+++ src/generics/CaixaIntTeste.java
@@ -3,10 +3,10 @@ package generics;
 public class CaixaIntTeste {
 	public static void main(String[] args) {
 		
-		CaixaInt<String> caixaA = new CaixaInt<String>();
-		caixaA.guardar("Teste");
+		CaixaInt caixaA = new CaixaInt();
+		caixaA.guardar(123);
 		
-		String coisaA = caixaA.abrir();
+		Integer coisaA = caixaA.abrir();
 		System.out.println(coisaA);
 	}
 }

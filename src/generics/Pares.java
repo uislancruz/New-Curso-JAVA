3e90c429d095bea328b928971d0d28c693bf907c
Uislan Cruz <uislansk8@hotmail.com>
2022-04-26 21:00:15 -0300

    Create Pares.java

---
+++ src/generics/Pares.java
@@ -0,0 +1,36 @@
+package generics;
+
+import java.util.Optional;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Pares<C extends Number, V> {
+	
+	private final SortedSet<Par<C, V>> itens = new TreeSet<>();
+	
+	public void adicionar(C chave, V valor) {
+		if(chave == null) return;
+		
+		Par<C, V> novoPar = new Par<C, V>(chave, valor);
+		
+		if(itens.contains(novoPar)) {
+			itens.remove(novoPar);
+		}
+		
+		itens.add(new Par<C, V>(chave, valor));
+	}
+	
+	public V getValor(C chave) {
+		if(chave == null) return;
+		
+		 	Optional<Par<C, V>> parOpcional =
+		 		itens.stream()
+				.filter(par -> chave.equals(par.getChave()))
+				.findFirst();
+		 	
+		 return parOpcional.isPresent()
+				? parOpcional.get().getValor() : null;
+	
+	}
+
+}

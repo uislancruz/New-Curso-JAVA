ae38abc3e1e808763bb9b03df82b29cb8ee0de4e
Uislan Cruz <uislansk8@hotmail.com>
2022-04-28 23:46:00 -0300

    Create Porteiro.java

---
+++ src/padroes/observer/Porteiro.java
@@ -0,0 +1,15 @@
+package padroes.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Porteiro {
+	
+	private List<ObservadorChegadaAniversariante> observadores 
+	= new ArrayList<>();
+	
+	public void registrarObservador(ObservadorChegadaAniversariante observador) {
+		observadores.add(observador);
+	}
+
+}

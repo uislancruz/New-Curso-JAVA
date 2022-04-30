cbed4c71c2bc78f8d50bbcf428a89b1e3a0857f1
Uislan Cruz <uislansk8@hotmail.com>
2022-04-30 11:21:12 -0300

    Update Porteiro.java

--- src/padroes/observer/Porteiro.java
+++ src/padroes/observer/Porteiro.java
@@ -1,7 +1,9 @@
 package padroes.observer;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Porteiro {
 	
@@ -10,6 +12,32 @@ public class Porteiro {
 	
 	public void registrarObservador(ObservadorChegadaAniversariante observador) {
 		observadores.add(observador);
+	}
+	
+	public void monitorar() {
+		Scanner entrada = new Scanner(System.in);
+		
+		String valor = "";
+		
+		while(!"sair".equalsIgnoreCase(valor)) {
+			System.out.print("Aniversariante chgeou? ");
+			
+			valor = entrada.nextLine();
+			
+			if("sim".equalsIgnoreCase(valor)) {
+				// criando evento
+				EventoChegadaAniversariante evento = 
+						new EventoChegadaAniversariante(new Date());
+				// notificar os observadores
+				observadores.stream()
+				.forEach(o -> o.chegou(evento));
+				
+			}else {
+				System.out.println("Alarme falso...");
+			}
+		}
+		
+		entrada.close();
 	}
 
 }

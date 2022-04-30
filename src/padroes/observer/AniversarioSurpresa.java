b37e6d7ce1e331f171278009d64f36a945c2c637
Uislan Cruz <uislansk8@hotmail.com>
2022-04-30 11:45:31 -0300

    Update AniversarioSurpresa.java

--- src/padroes/observer/AniversarioSurpresa.java
+++ src/padroes/observer/AniversarioSurpresa.java
@@ -5,8 +5,12 @@ public class AniversarioSurpresa {
 	public static void main(String[] args) {
 		Namorada namorada = new Namorada();
 		Porteiro porteiro = new Porteiro();
-		
 		porteiro.registrarObservador(namorada);
+		
+		porteiro.registrarObservador(e -> {
+			System.out.println("Supresa via lambda!");
+			System.out.println("Ocorreu em: " + e.getMomento());
+		});
 		porteiro.monitorar();
 	}
 

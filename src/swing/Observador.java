48e060a715c82b09733522793251b62da7d5127b
Uislan Cruz <uislansk8@hotmail.com>
2022-04-30 11:54:40 -0300

    Update Observador.java

--- src/swing/Observador.java
+++ src/swing/Observador.java
@@ -1,5 +1,8 @@
 package swing;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Observador {
@@ -7,6 +10,14 @@ public class Observador {
 	public static void main(String[] args) {
 		
 		JFrame janela = new JFrame("Observador");
+		janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		janela.setSize(600, 200);
+		
+		JButton botao = new JButton("Clicar! ");
+		janela.add(botao);
+		janela.setLayout(new FlowLayout());
+		
+		janela.setVisible(true);
 	}
 
 }

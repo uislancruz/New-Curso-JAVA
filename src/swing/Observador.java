bdca51768afb747dd504c21b981d2c7be8a92360
Uislan Cruz <uislansk8@hotmail.com>
2022-04-30 12:19:53 -0300

    Update Observador.java

--- src/swing/Observador.java
+++ src/swing/Observador.java
@@ -1,6 +1,8 @@
 package swing;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,11 +14,28 @@ public class Observador {
 		JFrame janela = new JFrame("Observador");
 		janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		janela.setSize(600, 200);
+		janela.setLayout(new FlowLayout());
+		janela.setLocationRelativeTo(null);
+		
 		
 		JButton botao = new JButton("Clicar! ");
 		janela.add(botao);
-		janela.setLayout(new FlowLayout());
 		
+		//fazendo o evento do botão com lambda
+		
+		botao.addActionListener(e ->{
+			System.out.println("Evento ocorreu!!!");
+		});
+		
+		//fazendo o evento do botao 
+		/*botao.addActionListener(new ActionListener() {
+						
+			public void actionPerformed(ActionEvent e) {
+			System.out.println("Evento ocorreu");
+				
+			}
+		});*/
+				
 		janela.setVisible(true);
 	}
 

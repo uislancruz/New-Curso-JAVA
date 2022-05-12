3f9ee88754d8ff8b964fb2e083b67f4e253b90dd
Uislan Cruz <uislansk8@hotmail.com>
2022-05-11 23:03:22 -0300

    Fazendo a conexão com MySql

--- src/jdbc/TestesConecxao.java
+++ src/jdbc/TestesConecxao.java
@@ -1,5 +1,22 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestesConecxao {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		final String url = "jdbc:mysql://localhost:3306";
+		final String usuario = "root";
+		final String senha = "tricolor";
+		
+		Connection conexao = DriverManager
+				.getConnection(url, usuario, senha);
+		
+		System.out.println("Conexão efetuada com sucesso");
+		conexao.close();
+	}
 
 }

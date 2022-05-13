568f6d6d1686409caac5d8dafc05976db531c4d7
Uislan Cruz <uislansk8@hotmail.com>
2022-05-13 20:30:21 -0300

    Update TestesConecxao.java

--- src/jdbc/TestesConecxao.java
+++ src/jdbc/TestesConecxao.java
@@ -8,15 +8,12 @@ public class TestesConecxao {
 	
 	public static void main(String[] args) throws SQLException{
 		
-		final String url = "jdbc:mysql://localhost:3306";
-		final String usuario = "root";
-		final String senha = "tricolor";
-		
-		Connection conexao = DriverManager
-				.getConnection(url, usuario, senha);
+		Connection conexao = FabricaConexao.getConexao();
 		
 		System.out.println("Conexão efetuada com sucesso");
 		conexao.close();
+		
 	}
-
+	
 }
+

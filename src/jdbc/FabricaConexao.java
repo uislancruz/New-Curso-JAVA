26d39b743e3d438b0277ce898bbfbec7082fdcbb
Uislan Cruz <uislansk8@hotmail.com>
2022-05-13 19:28:33 -0300

    Update FabricaConexao.java

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,5 +1,21 @@
 package jdbc;
 
-public class FabricaConexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class FabricaConexao {
+	
+	public static Connection getConexao() {
+		try {
+		final String url = "jdbc:mysql://localhost:3306";
+		final String usuario = "root";
+		final String senha = "tricolor";
+		
+		return DriverManager.getConnection(url, usuario, senha);
+	}catch (SQLException e) {
+		throw new RuntimeException(e);
+	}
+
+}
 }

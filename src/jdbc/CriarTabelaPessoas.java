cc3f463684830d8a2c532cfc0691f6a78a93ab2e
Uislan Cruz <uislansk8@hotmail.com>
2022-05-13 19:28:35 -0300

    Create CriarTabelaPessoas.java

---
+++ src/jdbc/CriarTabelaPessoas.java
@@ -0,0 +1,16 @@
+package jdbc;
+
+import java.sql.Connection;
+
+public class CriarTabelaPessoas {
+	
+	public static void main(String[] args) {
+		
+		Connection conexao = FabricaConexao.getConexao();
+		
+		String sql = "";
+		
+		
+	}
+
+}

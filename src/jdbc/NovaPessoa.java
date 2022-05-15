a6fdd567f9ab39ed7ccb0d9e15f57eea8c2a2daa
Uislan Cruz <uislansk8@hotmail.com>
2022-05-15 13:48:26 -0300

    Update NovaPessoa.java

--- src/jdbc/NovaPessoa.java
+++ src/jdbc/NovaPessoa.java
@@ -1,5 +1,7 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -10,10 +12,18 @@ public class NovaPessoa {
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Informe o nome: ");
-		String nome = entrada.next();
+		String nome = entrada.nextLine();
 		
-		String sql = "INSERT INTO pessoas (nome) VALUES (" + nome + ")";
+		Connection conexao = FabricaConexao.getConexao();
 		
+		String sql = "INSERT INTO pessoas (nome, codigo) VALUES (?, ?)";
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1, nome);
+		stmt.setInt(2, 100);
+		
+		stmt.execute();
+		
+		System.out.println("Pessoa incluida com sucesso!");
 		entrada.close();
 	}
 

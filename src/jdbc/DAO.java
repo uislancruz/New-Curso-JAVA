750587e661c9c3f9f8a4b13524f67df357b4c6c9
Uislan Cruz <uislansk8@hotmail.com>
2022-05-17 20:49:14 -0300

    Update DAO.java

--- src/jdbc/DAO.java
+++ src/jdbc/DAO.java
@@ -1,5 +1,26 @@
 package jdbc;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+public class DAO {
+	
+	private Connection conexao;
+	
+	private Connection getConexao(){
+		
+		try {
+			if(conexao != null && !conexao.isClosed()) {
+				return conexao;
+			}
+		} catch (SQLException e) {
+			
+			
+		}
+		
+		conexao = FabricaConexao.getConexao();
+		return conexao;
+	
+
+	}	
 }

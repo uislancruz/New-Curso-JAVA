7da25274d98f2c2d054e5846e4fae9fd73e001f1
Uislan Cruz <uislansk8@hotmail.com>
2022-05-17 20:13:08 -0300

    Criando um Doc para armazenar dados

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,5 +1,6 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,19 +12,21 @@ public class FabricaConexao {
 	public static Connection getConexao() {
 		
 		try {
-			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSl=true";
-			final String usuario = "root" ;
-			final String senha = "tricolor";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
 			
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException| IOException e) {
 			throw new RuntimeException(e);
 		}
 		
 
 	}
 	
-	private static Properties getProperties() throws SQLException{
+	private static Properties getProperties() throws IOException {
+		
 		Properties prop = new Properties();
 		String caminho = "/conexao.properties";
 		
